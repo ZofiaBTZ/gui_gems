59253a737c592c93bd1a6ce1f785c35e1ae2db32
ZofiaBTZ <zosia.baranczuk@gmail.com>
2016-05-12 10:49:31 +0200

    GuiGems
    Gui gems

---
+++ R/baseline_sex_age.R
@@ -0,0 +1,16 @@
+#' prepares a baseline function with sex and age of individuals 
+#' @name baselineFunction_sex_age
+#' @usage
+#' cohortSize <- 1000
+#' baseline <- baselineFunction_sex_age(cohortSize) # returns a data.frame with age and sex of individuals
+#' @author Zofia Baranczuk 
+#'  
+#' 
+#' @description This function prepares a data.frame baseline
+
+#'@export
+baselineFunction_sex_age <- function(cohortSize) {
+  age_lower_limit = 10
+  age_upper_limit = 60
+  data.frame(sex=rbinom(cohortSize, 1, .5), age=runif(cohortSize, age_lower_limit,age_upper_limit)) 
+}
---
+++ R/fixByString.R
@@ -0,0 +1,23 @@
+# @title fixing a function, the name can be given by a variable
+# @description
+# Like fix(). But the argument  
+# can be also as a value of a variable.
+# @param x - the name of an R object, as a name or a character string 
+# or a value of a variable.
+fixByString <- function (x, ...) 
+{
+  subx <- x
+  if (is.name(subx)) 
+    subx <- deparse(subx)
+  if (!is.character(subx) || length(subx) != 1L) 
+    stop("'fix' requires a name")
+  parent <- parent.frame()
+  if (exists(subx, envir = parent, inherits = TRUE)) 
+    x <- edit(get(subx, envir = parent), title = subx, ...)
+  else {
+    x <- edit(function() {
+    }, title = subx, ...)
+    environment(x) <- .GlobalEnv
+  }
+  assign(subx, x, envir = .GlobalEnv)
+}
